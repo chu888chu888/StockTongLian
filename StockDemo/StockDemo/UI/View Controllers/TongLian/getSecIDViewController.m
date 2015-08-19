f9b9ba863ce8986e61a22cd5c6fae2324906bd10
chuguangming <5211486@qq.com>
2015-08-19 13:39:31 +0800

    完成第一个基金基本信息查询的API

--- StockDemo/StockDemo/UI/View Controllers/TongLian/getSecIDViewController.m
+++ StockDemo/StockDemo/UI/View Controllers/TongLian/getSecIDViewController.m
@@ -12,6 +12,10 @@
 #import "FundInfo.h"
 @interface getSecIDViewController ()
 @property(nonatomic,strong) UITextField * txtticker;
+@property(nonatomic,strong) UITextField * txtsecShortName;
+@property(nonatomic,strong) UITextField * txtoperationMode;
+@property(nonatomic,strong) UITextField * txtestablishDate;
+@property(nonatomic,strong) UITextField * txtinvestTarget;
 @property(nonatomic,strong) UIButton * btnSearch;
 @property(nonatomic,strong) UITextView * txtResource;
 @end
@@ -23,6 +27,10 @@
     _txtticker=(UITextField *)[self.view viewWithTag:101];
     _btnSearch=(UIButton *)[self.view viewWithTag:102];
     _txtResource=(UITextView *)[self.view viewWithTag:103];
+    _txtsecShortName=(UITextField *)[self.view viewWithTag:104];
+    _txtoperationMode=(UITextField *)[self.view viewWithTag:105];
+    _txtestablishDate=(UITextField *)[self.view viewWithTag:106];
+    _txtinvestTarget=(UITextField *)[self.view viewWithTag:107];
     
     [_btnSearch addTarget:self action:@selector(btnSearch:) forControlEvents:UIControlEventTouchDown];
     _txtticker.text=@"000001";
@@ -61,7 +69,10 @@
                       NSLog(@"investField:%@",[responseObject[@"data"][0] objectForKey:@"investField"]);
                        */
                       _txtResource.text=[responseObject[@"data"][0] objectForKey:@"investField"];
-                      
+                      _txtsecShortName.text=[responseObject[@"data"][0] objectForKey:@"secShortName"];
+                      _txtoperationMode.text=[responseObject[@"data"][0] objectForKey:@"operationMode"];
+                      _txtestablishDate.text=[responseObject[@"data"][0] objectForKey:@"establishDate"];
+                      _txtinvestTarget.text=[responseObject[@"data"][0] objectForKey:@"investTarget" ];
                   }
                   failure:^(AFHTTPRequestOperation *operation, NSError *error) {
                       
